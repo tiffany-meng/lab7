cb51dd1d2e9c782c1892b739445d95cd979708e8
tiffany-meng <tmeng@ucsd.edu>
2021-02-16 16:54:44 -0800

    like button

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$("button.likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn() {
+	e.preventDefault();
+	ga("send", "event", 'like', 'click');
 }
